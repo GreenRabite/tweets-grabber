8d4fc5f2c199f21c63da9a402946e4fddf19ee19
Aluo <green.rabite@gmail.com>
2018-06-01 14:53:51 -0700

    Establish controller and Twitter Oauth to obtain user tweets. Generated Page

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,22 @@
 class SearchesController < ApplicationController
   def create
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["CONSUMER_KEY"]
+      config.consumer_secret     = ENV["CONSUMER_SECRET"]
+      config.access_token        = ENV["ACCESS_TOKEN"]
+      config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
+    end
+    @search = params[:search][:username]
+    begin
+      @tweets = @client.user_timeline("#{@search}",{count: 2})
+    rescue
+      render json: "Username does not exist or is private", status: 404
+    end
+    render 'static_pages/index'
   end
 
+  private
+  def search_params
+    params.require(:search).permit(:username)
+  end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,16 +1,4 @@
 class StaticPagesController < ApplicationController
   def index
-    #Hook for our webpage to inject in
-    @client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV["CONSUMER_KEY"]
-      config.consumer_secret     = ENV["CONSUMER_SECRET"]
-      config.access_token        = ENV["ACCESS_TOKEN"]
-      config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
-    end
-    p ENV
-    @client.user_timeline("GreenRabite").each do |tweet|
-      puts tweet.text
-    end
-    render :index
   end
 end
